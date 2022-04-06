5510f0f45aa884b0492f507e9322c6d3b0e410a0
Nde Yanick <yannisunique@gmail.com>
2022-04-06 17:00:19 +0100

    Smart contract done

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+
+
+
 module.exports = {
+  defaultNetwork: "kovan",
+  networks: {
+    hardhat: {
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad",
+      accounts: ["15d646881200aac55afef3e4a70c4c89e2a4545f6e7e4d824e074e42f0737018"],
+    }
+  },
+  etherscan: {
+  // Your API key for Etherscan
+  // Obtain one at https://etherscan.io/
+  apiKey: "TG4CX6ZJEU5VRFM9QRH23FD1DPNAPTIN5B"
+  },
   solidity: "0.8.13",
 };
